7cc4cf314b71371dbb018547d8747546f2252987
Valesca Moura <71470557+valescamoura@users.noreply.github.com>
2020-10-07 20:32:08 -0300

    ex6

---
+++ src/main/java/poo/lista3/exercicio3/ex6/TesteLampada.java
@@ -0,0 +1,36 @@
+/*Exercícios - Introdução a POO
+*Exercício 6
+*Escreva uma classe para representar uma lâmpada que está à venda em um
+*supermercado. Imagine que essa lâmpada possa ter três estados: apagada, acesa e
+*meia-luz. Além disso, ela tem uma operação “estaLigada” que retorne verdadeiro se
+*a lâmpada estiver ligada e falso, caso contrário. Crie uma classe TesteLampada que
+*implementa o método main com as seguintes operações:
+*a. Instancie 2 objetos dessa classe.
+*b. Ligar um dos objetos de Lampada e desligar o outro.
+*c. Mostrar na tela se as lâmpadas dos objetos estão ligadas ou desligadas*/
+
+package poo.lista3.exercicio3.ex6;
+
+public class TesteLampada {
+    public static void main(String[] args){
+        Lampada lampada1 = new Lampada();
+        Lampada lampada2 = new Lampada();
+        
+        lampada1.acender();
+        System.out.println("Lâmpada 1 após acender: ");
+        if(lampada1.estaLigada()) System.out.println("A lâmpada 1 está ligada!");
+        else System.out.println("A lâmpada 1 está desligada!");
+
+        lampada2.apagar();
+        System.out.println("Lâmpada 2 após apagar: ");
+        if(lampada2.estaLigada()) System.out.println("A lâmpada está 2 está ligada!");
+        else System.out.println("A lâmpada 2 está desligada!");
+        
+        lampada1.meiaLuz();
+        System.out.println("Lâmpada 1 após ligar meia-luz:");
+        lampada1.mostrarEstado();
+        lampada2.acender();
+        System.out.println("Lâmpada 2 após acender:");
+        lampada2.mostrarEstado();
+    }
+}
